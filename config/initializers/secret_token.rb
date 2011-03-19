d83fcade51b6ec549e6ce95075237bad0862e826
Brenda <brendateruel@aol.com>
2011-03-19 14:21:38 -0500

    added empty app files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trust::Application.config.secret_token = '73c0f826c2f3e57c197e6439cdf16e03dacfc6dd1b91a6104bdf36281ff66e9de12366e3a99f9ecd37e0ebd0a9309fcd64abaa77715c90c3500271b65014aa34'
