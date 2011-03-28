02346bebfbc348f410a0980c9480ff92977a0a5e
Brenda <brendateruel@aol.com>
2011-03-28 09:52:29 -0500

    added back field validators

--- app/models/ism.rb
+++ app/models/ism.rb
@@ -1,6 +1,6 @@
 class Ism < ActiveRecord::Base
   
-  validates :description, :gender, :presence => true
+  validates :description, :gender, :category, :presence => true
   validates :description, :length => { :maximum => 140 }
   validates :description, :uniqueness => true
   	
