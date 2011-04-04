4fc2770df9ed3dfd9fcb7ac1731f4ba65b64f7db
Brenda <brendateruel@aol.com>
2011-04-03 21:41:22 -0500

    javacript files added to folder

---
+++ public/javascripts/submit-form.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function(){
+	$("#submit").hide();
+	$("#submit-box a").click(function(){
+	    $("#submit-box a").toggleClass("toggle");
+	    $("#submit").slideToggle("fast");
+		return false;
+	});
+	
+	$("input[type=text]").each(function() {
+        var search = 'Never Trust Someone Who...';
+        var empty = '';
+        $(this).attr('value', search);
+        $(this).focus(function() {
+            if (this.value == search) {
+                this.value = empty;
+            }
+        });
+        $(this).blur(function() {
+            if (this.value == empty) {
+                this.value = search;
+            }
+        });
+    });
+
+});
